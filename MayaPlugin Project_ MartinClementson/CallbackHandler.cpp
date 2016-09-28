2aac2ec78c6c8765d90828d536e5c933b03e7d32
MartinClementson <martinc.91@hotmail.com>
2016-09-28 16:01:59 +0200

    Callbacks are a workin

--- MayaPlugin Project_ MartinClementson/CallbackHandler.cpp
+++ MayaPlugin Project_ MartinClementson/CallbackHandler.cpp
@@ -1,10 +1,10 @@
+
 #include "CallbackHandler.h"
-
-
 
 CallbackHandler::CallbackHandler()
 {
 }
+
 
 
 CallbackHandler::~CallbackHandler()
@@ -13,8 +13,7 @@ CallbackHandler::~CallbackHandler()
 
 }
 
-
-bool CallbackHandler::Init(circular_buffer<char>*)
+bool CallbackHandler::Init()
 {
 	MStatus result = MS::kSuccess;
 
@@ -22,6 +21,7 @@ bool CallbackHandler::Init(circular_buffer<char>*)
 	id = MDGMessage::addNodeAddedCallback(NodeCreated, "dependNode", NULL, &result);
 	if (MFAIL(result))
 		return false;
+
 	callBackIds.append(id);
 
 
@@ -35,6 +35,7 @@ bool CallbackHandler::Init(circular_buffer<char>*)
 		MDagPath path = MDagPath::getAPathTo(thisTransform.child(0));
 
 		MCallbackId newId = MDagMessage::addWorldMatrixModifiedCallback(path, WorldMatrixChanged, NULL, &result);
+
 		if (result == MS::kSuccess)
 		{
 			if (callBackIds.append(newId) == MS::kSuccess)
@@ -53,7 +54,7 @@ bool CallbackHandler::Init(circular_buffer<char>*)
 	{
 
 		MFnMesh thisMesh(meshIt.currentItem());
-		
+
 		MCallbackId polyId = MNodeMessage::addAttributeChangedCallback(meshIt.currentItem(), VertChanged, NULL, &result);
 
 		//thisMesh.attribute("quadSplit",) <---- IMPORTANT! find out how to!
@@ -77,4 +78,93 @@ bool CallbackHandler::Init(circular_buffer<char>*)
 
 
 	return true;
+}
+
+void CallbackHandler::VertChanged(MNodeMessage::AttributeMessage msg, MPlug & plug, MPlug & otherPlug, void *)
+{
+
+	if (msg & MNodeMessage::AttributeMessage::kAttributeSet && !plug.isArray() && plug.isElement()) //if a specific vert has changed
+	{
+		MStringArray changes;
+		MFnNumericData point(plug.attribute());
+
+
+		plug.getSetAttrCmds(changes, MPlug::kChanged);
+		if (changes.length() == 1)
+		{
+
+
+			float x, y, z;
+			point.getData3Float(x, y, z);
+			x = plug.child(0).asFloat();
+			y = plug.child(1).asFloat();
+			z = plug.child(2).asFloat();
+			std::cerr << "A Vert has changed!! |" << x << "," << y << "," << z << "|  " << changes << std::endl;
+
+		}
+	}
+}
+
+void CallbackHandler::WorldMatrixChanged(MObject & transformNode, MDagMessage::MatrixModifiedFlags & modified, void * clientData)
+{
+
+	MFnTransform obj(transformNode);
+	MMatrix matrix = obj.transformationMatrix();
+	std::cerr << matrix.matrix[0][0] << " " << matrix.matrix[0][1] << " " << matrix.matrix[0][2] << " " << matrix.matrix[0][3] << std::endl;
+	std::cerr << matrix.matrix[1][0] << " " << matrix.matrix[1][1] << " " << matrix.matrix[1][2] << " " << matrix.matrix[1][3] << std::endl;
+	std::cerr << matrix.matrix[2][0] << " " << matrix.matrix[2][1] << " " << matrix.matrix[2][2] << " " << matrix.matrix[2][3] << std::endl;
+	std::cerr << matrix.matrix[3][0] << " " << matrix.matrix[3][1] << " " << matrix.matrix[3][2] << " " << matrix.matrix[3][3] << std::endl;
+
+	std::cerr << "A TransformNode has changed!! |" << obj.name() << "   | " << modified << std::endl;
+}
+
+void CallbackHandler::TopologyChanged(MObject & node, void * clientData)
+{
+
+	MFnMesh thisMesh(node);
+
+	std::cerr << "Topology has changed |" << thisMesh.name() << std::endl;
+}
+
+void CallbackHandler::NodeCreated(MObject & node, void * clientData)
+{
+
+	if (node.hasFn(MFn::kMesh))
+	{
+
+		MFnDagNode nodeHandle(node);
+		MStatus result;
+		MCallbackId polyId = MNodeMessage::addAttributeChangedCallback(node, VertChanged, NULL, &result);
+
+
+		if (MS::kSuccess == result)
+		{
+
+
+			callBackIds.append(polyId);
+			std::cerr << "A new node was created \n";
+			std::cerr << "Callback triggered from  node : " << nodeHandle.name() << "\n"
+				<< "Node Path : " << nodeHandle.fullPathName() << "\n" << std::endl;
+
+			MDagPath path = MDagPath::getAPathTo(node);
+			polyId = MDagMessage::addWorldMatrixModifiedCallback(path, WorldMatrixChanged, NULL, &result);
+			if (result == MS::kSuccess)
+			{
+				callBackIds.append(polyId);
+			}
+			MCallbackId polyId = MPolyMessage::addPolyTopologyChangedCallback(node, TopologyChanged, NULL, &result);
+			if (result == MS::kSuccess)
+			{
+				callBackIds.append(polyId);
+				std::cerr << "Polychange callback added!  " << node.apiTypeStr() << std::endl;
+			}
+
+		}
+
+
+
+	}
+
+
+
 }
